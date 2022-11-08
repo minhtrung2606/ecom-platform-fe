0dedc3f19fc9b0d2fd8c55fc777444bf1283e4f4
minhtrung2606 <minhtrung2606@gmail.com>
2022-11-08 20:25:07 +0700

    Remove unused code

--- components/base/Image/styles.js
+++
@@ -1,9 +0,0 @@
-import Image from 'next/image'
-import styled from 'styled-components'
-import { CollorPalette } from '../../../libs/Colors'
-
-export const StyledImage = styled(Image)`
-  background-color: ${CollorPalette.BackgroundColor};
-  object-fit: ${({ objectfit }) =>
-    objectfit === 'true' ? 'contain' : 'cover'};
-`
