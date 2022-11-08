64f790136d64ffcba6c18e9bb9d567edf1a817d1
minhtrung2606 <minhtrung2606@gmail.com>
2022-11-08 20:11:17 +0700

    Fix error of classname is mismatch when using styled components

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  compiler: {
+    // https://www.appsloveworld.com/reactjs/100/31/warning-prop-classname-did-not-match-in-next-js-site-with-styled-components
+    styledComponents: process.env.NODE_ENV !== 'production',
+  },
 }
 
 module.exports = nextConfig
