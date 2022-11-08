a3daeb5d80d840b9dbe8475956cc2a47fae3a1ba
minhtrung2606 <minhtrung2606@gmail.com>
2022-11-08 20:21:44 +0700

    Refactor AppHeader

--- components/containers/AppHeader.js
+++ components/containers/AppHeader.js
@@ -1,5 +1,14 @@
-import { Container } from '../../styles'
-import { AddHeaderContainer, StyledLogo } from './styles'
+import styled from 'styled-components'
+import { Container } from '../styles'
+
+const AddHeaderContainer = styled.div`
+  padding: 1.5rem 0 1rem;
+`
+
+export const StyledLogo = styled.img`
+  display: inline-block;
+  width: 9rem;
+`
 
 const AppHeader = () => (
   <Container data-testid="app-header">
