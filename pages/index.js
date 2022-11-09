84d6e6eb8981b100f8a16e92b622fe6151abc4d5
Steve <steve.t@ad-i.com>
2022-11-09 17:15:11 +0700

    Implement the ProductCardList component

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,27 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from '../components/App'
 import Banners from '../components/Banners'
 import Card from '../components/Card'
 import FlexWrapper from '../components/FlexWrapper'
 import Section from '../components/Section'
 import { CollorPalette } from '../libs/Colors'
+import ProductCardList from '../components/ProductCardList';
 
 export default function Home() {
   return (
     <App>
       <Banners />
       <Section title="Sản phẩm mới">
-        <div>Section(Sản phẩm mới)</div>
+        {/*<div>Section(Sản phẩm mới)</div>*/}
+        <ProductCardList count={4} />
       </Section>
       <Section title="Giao nhanh Quận 7">
-        <div>Section(Giao nhanh Quận 7)</div>
+        {/*<div>Section(Giao nhanh Quận 7)</div>*/}
+        <ProductCardList count={6} />
       </Section>
       <Section title="Quan tâm nhiều nhất">
-        <div>Section(Quan tâm nhiều nhất)</div>
+        {/*<div>Section(Quan tâm nhiều nhất)</div>*/}
+        <ProductCardList count={7} />
       </Section>
       <Section
         variants={{
